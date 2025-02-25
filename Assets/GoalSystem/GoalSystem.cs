591ce18494f13e03eda0a50bd10ef315caf48978
rinngo <73386339+rinngo0302@users.noreply.github.com>
2025-02-25 20:34:17 +0800

    goal system

---
+++ Assets/GoalSystem/GoalSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalSystem : MonoBehaviour
+{
+    [SerializeField] private HungerSystem _hungerSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_hungerSystem == null)
+        {
+            Debug.LogError("HungerSystem is not attached");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFinish())
+        {
+            SceneManager.LoadScene("end");
+        }
+    }
+
+    private bool IsFinish()
+    {
+        int hunger = _hungerSystem.Hunger;
+
+        return hunger <= 0;
+    }
+}
